a92d99f72e87b99893037bec67bbb4a55de69774
navabhi22 <navabhi22@gmail.com>
2022-03-11 00:47:22 +0530

    [Abhinav] Added bubble sort method

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -6,14 +6,31 @@ namespace AlgorithmProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Problems Program");
 
-            //Problem-4
-
-            int[] arr = { 1, 2, 6, 4, 3, 6, 7, 9, };
-            InsertionSort insertionSort = new InsertionSort();
-            insertionSort.Sort(arr);
-
+            int[] arr = new int[10] { 23, 98, 45, 12, 9, 16, 65, 74, 34, 29 };//Array created with 10 unsorted numbers
+            bubblesort(arr, 10);
+            int a;
+            for (a = 0; a < 10; a++)
+                Console.Write(arr[a] + "\t");//Printing the array after sorting in descending order
+            Console.ReadLine();
+        }
+        //bubble sort
+        static void bubblesort(int[] data, int n)
+        {
+            int a;
+            int b;
+            for (a = 0; a < n; a++)
+                for (b = n - 1; b > a; b--)
+                    if (data[b] > data[b - 1])//Condition to arrange array in descending order
+                    {
+                        int temp = data[b];
+                        data[b] = data[b - 1];
+                        data[b - 1] = temp;
+                    }
         }
     }
+
+
+
 }
+    
