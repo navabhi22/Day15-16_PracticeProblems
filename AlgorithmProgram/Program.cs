7a51287ca0900ea69844b9fb17f1053f3cc9c50c
navabhi22 <navabhi22@gmail.com>
2022-03-10 23:52:40 +0530

    Greeting message for Algorithm Programs

---
+++ AlgorithmProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Problems Program");
+        }
+    }
+}
