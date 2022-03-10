6055b1f0b10a5e8d3a9bc8b264144acece640901
navabhi22 <navabhi22@gmail.com>
2022-03-11 00:56:48 +0530

    [Abhinav] Updated Program.cs

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -6,31 +6,9 @@ namespace AlgorithmProgram
     {
         static void Main(string[] args)
         {
+            PrimeNumber.PrimeNum(1, 10);
+        }
 
-            int[] arr = new int[10] { 23, 98, 45, 12, 9, 16, 65, 74, 34, 29 };//Array created with 10 unsorted numbers
-            bubblesort(arr, 10);
-            int a;
-            for (a = 0; a < 10; a++)
-                Console.Write(arr[a] + "\t");//Printing the array after sorting in descending order
-            Console.ReadLine();
-        }
-        //bubble sort
-        static void bubblesort(int[] data, int n)
-        {
-            int a;
-            int b;
-            for (a = 0; a < n; a++)
-                for (b = n - 1; b > a; b--)
-                    if (data[b] > data[b - 1])//Condition to arrange array in descending order
-                    {
-                        int temp = data[b];
-                        data[b] = data[b - 1];
-                        data[b - 1] = temp;
-                    }
-        }
     }
-
-
-
 }
     
