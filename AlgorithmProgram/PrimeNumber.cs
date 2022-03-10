5b0145467dcc1c81b1a8853e67f89a1e99b7a04c
navabhi22 <navabhi22@gmail.com>
2022-03-11 00:56:28 +0530

    [Abhinav] Added PrimeNumber method

---
+++ AlgorithmProgram/PrimeNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class PrimeNumber
+    {
+        public static void PrimeNum(int N1, int N2)
+        {
+            for (int j = N1; j <= N2; j++)
+            {
+                int numOfFact = 0;
+                for (int i = 1; i <= j; i++)
+                {
+                    int rimainder = j % i;
+
+                    if (rimainder == 0)
+                    {
+                        numOfFact++;
+                    }
+
+                }
+
+                if (numOfFact == 2)
+                {
+                    //return (true);
+                    Console.WriteLine("{0} is prime "+j );
+                }
+                else
+                {
+                    //return (false);
+                    Console.WriteLine("{0} not prime " +j);
+                }
+            }
+        }
+    }
+}
